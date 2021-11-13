aa28a1aaa6458befdb14172f168483536e27b6ba
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2021-11-13 22:37:59 +0100

    Create abs_config.c

---
+++ abs_config.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+   int p = abs(100);
+   int n = abs(-100);
+ 
+   printf("La valeur absolue de 100 est %d\n", p);
+   printf("La valeur absolue de -100 est %d \n",n);
+   return 0;
+}
